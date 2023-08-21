3132e444e605911a80cceff6e834caea25a8c97b
Kojobio <53830751+kojobio@users.noreply.github.com>
2023-08-21 01:53:27 +0000

    Add files via upload

--- firebase.js
+++ firebase.js
@@ -1,7 +1,6 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.2.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.2.0/firebase-auth.js";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
